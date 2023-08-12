14e5c9b6c2cd7352e5856ef2b44ae43f9fcb9fb0
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-08-12 20:47:33 +0200

    added promises to main functions

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // file system module to work with json file
 const fs = require("fs");
 
-const AccountIndexer = require('./AccountIndexer');
+const AccountIndexer = require("./AccountIndexer");
 
 const accountUpdates = require("./coding-challenge-input.json");
 
 const accountIndexer = new AccountIndexer();
 
 // simulate real-time updates
-for (const accountUpdate of accountUpdates) {
-  const randomDelay = Math.floor(Math.random() * 1000);
-  setTimeout(
-    () => accountIndexer.ingestAccountUpdate(accountUpdate),
-    randomDelay
-  );
+async function simulateUpdates() {
+  for (const accountUpdate of accountUpdates) {
+    const randomDelay = Math.floor(Math.random() * 1000);
+    await new Promise((resolve) => setTimeout(resolve, randomDelay));
+    await accountIndexer.ingestAccountUpdate(accountUpdate);
+  }
 }
 
 // simulate the shutdown after all updates have been processed
-setTimeout(() => accountIndexer.shutdown(), 10000);
+async function simulateShutdown() {
+  await simulateUpdates();
+  setTimeout(() => accountIndexer.shutdown(), 10000);
+}
+
+simulateShutdown();
